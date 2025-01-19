e06a0bfb48c98c28fbaa71a818532c14139015b0
lmace-eriks <lucasmace4130@gmail.com>
2025-01-19 13:29:36 -0600

    First Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "not-found.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "public"));
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
+
+const port = process.env.PORT || 3002;
+app.listen(port, () => console.log(`\n ❤️  Server listening on port: ${port} ❤️ \n`));
+
+export default app;
