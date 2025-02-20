0a84fd49d083fc3ba89f1049aa8706c4d1f86634
lmace-eriks <lucasmace4130@gmail.com>
2025-02-19 21:05:25 -0600

    Add about page. Scaffold CSS

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -5,9 +5,10 @@ export default function Header() {
     return (
         <header className={s.header}>
             <nav>
-                <Link href={"#"}>Home</Link>
-                <Link href={"#"}>About</Link>
-                <Link href={"#"}>Contact</Link>
+                <Link href="./">Home</Link>
+                <Link href="./about-stupor-bowl">About Stupor Bowl</Link>
+                <Link href="#">Contact</Link>
+                <Link href="#" className="ml-auto">Right Side Link</Link>
             </nav>
         </header>
     )
