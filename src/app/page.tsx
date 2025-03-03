cbdea2301857d691c4f301767289364ffa63de2a
lmace-eriks <lucasmace4130@gmail.com>
2025-03-02 18:29:27 -0600

    Create archive directory structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,6 @@ export default function Home() {
       <h1>Hello, Minneapolis!</h1>
       <Image src="/images/lake-billy-16-9.webp" width={2342} height={1317} loading="eager" fetchPriority="high" alt="A bicycle racer on a frozen lake." className={s.hero} />
 
-      <article>
-        <ImageAsset
-          src="/images/stupor-17-patch.jpg"
-          width={3072}
-          height={4080}
-          alt="Torn piece of clothing with two patches sewn on."
-          credit={{ owner: "Anthony Ross" }}
-        >
-          <figcaption>Scrap saved from a battle vest of the owner. Genuine patch from Stupor Bowl 17. Also pictured is a patch from the now defunt <i>First Avenue Cycling Club</i> of Minneapolis.</figcaption>
-        </ImageAsset >
-      </article>
     </>
   );
 }
